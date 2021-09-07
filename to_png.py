500d5f70fa17bd99094c9ed3eff93591f23eef64
laijackylai <66239914+laijackylai@users.noreply.github.com>
2021-09-07 16:13:17 +0800

    successfully translated dataset to height map

--- to_png.py
+++ to_png.py
@@ -1,5 +1,6 @@
 import time
 import os
+from numpy.lib.type_check import imag
 import pandas as pd
 import numpy as np
 from osgeo import gdal
@@ -7,31 +8,38 @@ import rasterio as rio
 from rasterio.warp import calculate_default_transform, reproject, Resampling
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
+from PIL import Image
+import rasterio as rio
 
 inpath = os.getcwd() + "/tif/"
 outpath = os.getcwd() + "/png/"
 
 os.chdir(inpath)
 files = [f for f in os.listdir(
-    inpath) if os.path.isfile(os.path.join(inpath, f))]
+    inpath) if os.path.isfile(os.path.join(inpath, f)) and '.tif' in f]
 
 for f in files:
     print(f)
+    with rio.open(inpath + f) as src:
+        print(src.bounds)
+
     lu = gdal.Open(inpath + f)
     raw_data = lu.ReadAsArray()
-    print(np.shape(raw_data))
-    exit()
 
-    lue = lu.GetGeoTransform()
-    Proj = lu.GetProjection()
+    image = Image.fromarray(raw_data).convert('LA')
+    name = str(f.split('.')[0])
+    image.save(outpath + name + '.png')
 
-    band = lu.GetRasterBand(1)
-    array = band.ReadAsArray()
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    img = ax.imshow(array)
-    img.set_cmap('jet')
-    fig.colorbar(img)
-    # plt.plot(xlonx,ylatx,'md')
-    name = f.split('.')[0]
-    fig.savefig(outpath + name + ".png")
+    # lue = lu.GetGeoTransform()
+    # Proj = lu.GetProjection()
+
+    # band = lu.GetRasterBand(1)
+    # array = band.ReadAsArray()
+    # fig = plt.figure()
+    # ax = fig.add_subplot(1, 1, 1)
+    # img = ax.imshow(array)
+    # img.set_cmap('jet')
+    # fig.colorbar(img)
+    # # plt.plot(xlonx,ylatx,'md')
+    # name = f.split('.')[0]
+    # fig.savefig(outpath + name + ".png")
--- txt_to_geotif.py
+++ txt_to_geotif.py
@@ -2,6 +2,7 @@
 ### This python file is used to translate all the xyz TXT file into Geotiff.                    ###
 ###################################################################################################
 # import the needed package
+import math
 import time
 import os
 import pandas as pd
@@ -12,6 +13,25 @@ from rasterio.warp import calculate_default_transform, reproject, Resampling
 from pyproj import Transformer
 
 
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance in kilometers between two points
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(math.radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = math.sin(dlat/2)**2 + math.cos(lat1) * \
+        math.cos(lat2) * math.sin(dlon/2)**2
+    c = 2 * math.asin(math.sqrt(a))
+    # Radius of earth in kilometers. Use 3956 for miles. Determines return value units.
+    r = 6371
+    return c * r * 1000  # in meters
+
+
 # set paths
 inpath = os.getcwd() + "/input_txt/"
 outpath = os.getcwd() + "/tif/"
@@ -20,18 +40,20 @@ start_time = time.time()
 
 # make file list
 os.chdir(inpath)
+
 files = [f for f in os.listdir(
     inpath) if os.path.isfile(os.path.join(inpath, f)) and 'test' not in f and 'DS_Store' not in f]
 
 for f in files:
     # open .xyz file as csv file
     start_time2 = time.time()
-    print(f)
+    print('processing ' + f)
     df = pd.read_csv(inpath + f, sep=",")
     df = df.dropna(axis=1)
     df = pd.DataFrame(np.vstack([df.columns, df]))
     for i in [0, 1, 2, 3]:
         df.iloc[0][i] = float(df.iloc[0][i])
+    # should be Easting, Northing
     df.columns = ["Easting", "Northing", "Ele", "Na", "Na2"]
     name = f.split('.')[0]
     df = df.drop(columns={'Na', 'Na2'})
@@ -41,17 +63,23 @@ for f in files:
     # reference: https://github.com/shermanfcm/HK1980#python
     lat, lon = transformer.transform(df['Northing'], df['Easting'])
 
-    #! Insert the translated lat and lon to the pandas dataframe
-    df = df.insert(0, 'Lon', lon)
-    df = df.insert(0, 'Lat', lat)
-    print(df)
+    # #! Insert the translated lat and lon to the pandas dataframe
+    df.insert(0, 'Lon', lon.tolist())
+    df.insert(0, 'Lat', lat.tolist())
 
-    # df.to_csv(outpath + name + ".csv", index=False)
-    # print('Translated to CSV')
+    df.to_csv(outpath + name + ".csv", index=False)
+    print('Translated to CSV')
 
-    # print('lat range: ' + str(df['Lat'].min()) + ' -> ' + str(df['Lat'].max()))
-    # print('lon range: ' + str(df['Lon'].min()) + ' -> ' + str(df['Lon'].max()))
-    exit()
+    minLat = lat.min()
+    maxLat = lat.max()
+    minLon = lon.min()
+    maxLon = lon.max()
+
+    lat_cover = haversine(minLon, minLat, minLon, maxLat)
+    lon_cover = haversine(minLon, minLat, maxLon, minLat)
+
+    img_width = lat_cover / 2
+    img_height = lon_cover / 2
 
     # create .vrt file for raster format conversion (from csv to Geotiff(gdal))
     os.chdir(outpath)
@@ -60,53 +88,56 @@ for f in files:
     lyr_name = fn.replace('.csv', '')
     out_tif = fn.replace('.csv', '.tif')
 
-    with open(outpath + vrt_fn, 'w') as fn_vrt:
+    with open(outpath + vrt_fn, 'w+') as fn_vrt:
         fn_vrt.write('<OGRVRTDataSource>\n')
         fn_vrt.write('\t<OGRVRTLayer name="%s">\n' % lyr_name)
         fn_vrt.write('\t\t<SrcDataSource>%s</SrcDataSource>\n' % fn)
         fn_vrt.write('\t\t<GeometryType>wkbPoint</GeometryType>\n')
         fn_vrt.write(
-            '\t\t<GeometryField encoding="PointFromColumns" x="Lon" y="Lat" z="Ele"/>\n')
+            '\t\t<GeometryField encoding="PointFromColumns" x="Lat" y="Lon" z="Ele"/>\n')
         fn_vrt.write('\t</OGRVRTLayer>\n')
         fn_vrt.write('</OGRVRTDataSource>\n')
 
-    gdal.Grid(outpath + out_tif, outpath + vrt_fn)
+    print('created VRT')
 
-    os.remove(outpath + fn)  # remove the csv file
+    gridOptions = {
+        'destName': outpath + out_tif,
+        'srcDS': outpath + vrt_fn,
+        'width': img_width,
+        'height': img_height
+    }
+
+    gdal.Grid(**gridOptions)
+
+    # os.remove(outpath + fn)  # remove the csv file
     # os.remove(outpath + vrt_fn)  # remove the vrt file
-    print("--- %s seconds ---" % (time.time() - start_time2))
+    print('created GeoTiff')
 
-print("--- %s seconds ---" % (time.time() - start_time))
-print("--- CSV to GTiff convertion DONE ---")
+# # find the Tiff file to loop for
+# tif_files = [f for f in os.listdir(
+#     outpath) if os.path.isfile(os.path.join(outpath, f)) and 'test' not in f and 'DS_Store' not in f]
 
+# # set the coordinate system
+# dst_crs = 'EPSG:2326'
+# # change the coordinate system in each file
+# for tfn in tif_files:
+#     print(tfn)
+#     with rio.open(outpath + tfn) as src:
+#         # print(src.crs, dst_crs, src.width, src.height, *src.bounds)
+#         transform, width, height = calculate_default_transform(
+#             src.crs, dst_crs, src.width, src.height, *src.bounds)
+#         kwargs = src.meta.copy()
+#         kwargs.update({'crs': dst_crs, 'transform': transform,
+#                       'width': width, 'height': height})
+#         print(kwargs)
+#         with rio.open(outpath + tfn, 'w', **kwargs) as dst:
+#             for i in range(1, src.count + 1):
+#                 reproject(source=rio.band(src, i),
+#                           destination=rio.band(dst, i),
+#                           src_transform=src.transform,
+#                           src_crs=src.crs,
+#                           dst_transform=transform,
+#                           dst_crs=dst_crs,
+#                           resampling=Resampling.nearest)
 
-start_time1 = time.time()
-
-# find the Tiff file to loop for
-tif_files = [f for f in os.listdir(
-    outpath) if os.path.isfile(os.path.join(outpath, f))]
-
-# set the coordinate system
-dst_crs = 'EPSG:2326'
-# change the coordinate system in each file
-for tfn in tif_files:
-    print(tfn)
-    with rio.open(outpath + tfn) as src:
-        transform, width, height = calculate_default_transform(
-            src.crs, dst_crs, src.width, src.height, *src.bounds)
-        kwargs = src.meta.copy()
-        kwargs.update({'crs': dst_crs, 'transform': transform,
-                      'width': width, 'height': height})
-        with rio.open(outpath + tfn, 'w', **kwargs) as dst:
-            for i in range(1, src.count + 1):
-                reproject(source=rio.band(src, i),
-                          destination=rio.band(dst, i),
-                          src_transform=src.transform,
-                          src_crs=src.crs,
-                          dst_transform=transform,
-                          dst_crs=dst_crs,
-                          resampling=Resampling.nearest)
-print("--- %s seconds ---" % (time.time() - start_time1))
-print("--- GTiff UPDATE coordinate system DONE ---")
-
-print("--- Total: %s seconds ---" % (time.time() - start_time))
+# print('updated GeoTiff coordinate system')
