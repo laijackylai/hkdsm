b125a36b1f2b123192d05afd3edf24b5634e7763
laijackylai <66239914+laijackylai@users.noreply.github.com>
2022-03-26 18:35:41 +0800

    add support for returning mapbox rgb terrain outside hk

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http2Express = require('http2-express-bridge')
 const http2 = require('http2')
 const cors = require('cors')
 const fs = require('fs')
+const request = require('request')
 const { promisify } = require('util')
 
 const readFile = promisify(fs.readFile)
@@ -14,14 +15,29 @@ app.get('/tiles/:z-:x-:y.png', async (req, res) => {
   const { z, x, y } = req.params
   const imgPath = `tiles/png/${z}/${x}-${y}-${z}.png`
 
-  console.info(req.headers['user-agent'], ': ', req.headers[':method'], imgPath)
+  // console.info(req.headers['user-agent'], ': ', req.headers[':method'], imgPath)
 
   if (fs.existsSync(imgPath)) {
     const img = await readFile(imgPath)
     res.send(img)
-  }
-  else {
-    res.send('file not found')
+    console.info('sent hkdsm', img, '\n')
+  } else {
+    const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoibGFpamFja3lsYWkiLCJhIjoiY2tjZWZucjAzMDd1eDJzcGJvN2tiZHduOSJ9.vWThniHwg9V1wEO3O6xn_g';
+    const mapboxRGBUrl = `https://api.mapbox.com/v4/mapbox.terrain-rgb/${z}/${x}/${y}.pngraw?access_token=${MAPBOX_ACCESS_TOKEN}`
+
+    const options = {
+      url: mapboxRGBUrl,
+      method: "get",
+      encoding: null
+    };
+
+    request(options, (error, response, body) => {
+      if (error) {
+        return
+      }
+      res.send(body)
+      console.info('sent mapbox terrain', body, '\n')
+    })
   }
 })
 
